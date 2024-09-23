9a8a791ed3e2ca1d34ec30bd02882cc200433052
Andrii Nechaiev <andreynech@aswsyst.cz>
2024-09-23 16:18:48 +0000

    integrate codecov

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,9 @@
 import nox
 
 
+nox.options.sessions = ["lint", "tests"]
+
+
 @nox.session(python=["3.9", "3.11", "3.12"])
 def tests(session):
     """Run tests."""
@@ -15,3 +18,10 @@ def lint(session):
     """Run linting."""
     session.run("make", "lintdeps", external=True)
     session.run("make", "lint", external=True)
+
+
+@nox.session(python="3.12")
+def coverage(session):
+    """Run coverage."""
+    session.run("make", "testdeps", external=True)
+    session.run("pytest", "--cov=csobpg", "--cov-report=xml")
